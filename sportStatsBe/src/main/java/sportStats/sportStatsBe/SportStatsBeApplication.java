e3ba2e5ad616d58d23e619a509f8d198ecb15589
lorenzo.coppola <lorenzo.coppola@eositaly.it>
2023-05-22 21:01:54 +0200

    first commit be

---
+++ sportStatsBe/src/main/java/sportStats/sportStatsBe/SportStatsBeApplication.java
@@ -0,0 +1,13 @@
+package sportStats.sportStatsBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportStatsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportStatsBeApplication.class, args);
+	}
+
+}
---
+++ sportStatsBe/src/test/java/sportStats/sportStatsBe/SportStatsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package sportStats.sportStatsBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportStatsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
