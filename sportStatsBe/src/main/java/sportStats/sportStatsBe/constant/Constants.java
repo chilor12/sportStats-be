4131f5d02d69ce99b067a2e9e0e638dc168b7d8e
chilor12 <lore18coppola@gmail.com>
2023-06-29 12:40:59 +0200

    add constant

---
+++ sportStatsBe/src/main/java/sportStats/sportStatsBe/constant/Constants.java
@@ -0,0 +1,14 @@
+package sportStats.sportStatsBe.constant;
+
+public class Constants {
+
+	private Constants() {
+
+    }
+
+    public static final int QUERY_NUM_PAGE = 10;
+
+    public static final String URL_SYSTEMA = "url.sistema";
+
+	
+}
